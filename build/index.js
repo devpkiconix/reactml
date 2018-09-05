dc08863c48d3610fba87b71fd8239a6284d42cc8
Prasanna Kondapaneni <prasanna@teamiconix.com>
2018-09-05 14:03:03 +0530

    Added a few more exports in main index file Removed codegen because "prettier" is causing some webpack build issues.

--- build/index.js
+++ build/index.js
@@ -10,16 +10,20 @@ var _ReactMLHoc = require('./components/reactml/ReactMLHoc');
 
 var _ReactMLHoc2 = _interopRequireDefault(_ReactMLHoc);
 
-var _codegen = require('./tools/codegen');
-
-var _codegen2 = _interopRequireDefault(_codegen);
-
 var _reactml = require('./modules/reactml');
 
 var _reactml2 = _interopRequireDefault(_reactml);
 
+var _util = require('./modules/reactml/util');
+
+var _materialUiTagFactory = require('./components/reactml/materialUiTagFactory');
+
+var _materialUiTagFactory2 = _interopRequireDefault(_materialUiTagFactory);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
-  ReactML: _ReactML.ReactML, ReactMLHoc: _ReactMLHoc2.default, reducer: _reactml2.default, codegen: _codegen2.default
+  ReactML: _ReactML.ReactML, ReactMLHoc: _ReactMLHoc2.default, reducer: _reactml2.default,
+  materialUiTagFactory: _materialUiTagFactory2.default,
+  reactmlFieldChangeHandler: _util.reactmlFieldChangeHandler, fromYaml: _util.fromYaml, toYaml: _util.toYaml, dispatchFieldUpdate: _util.dispatchFieldUpdate
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { ReactML, } from './components/reactml/ReactML';
 import ReactMLHoc from './components/reactml/ReactMLHoc';
-import codegen from './tools/codegen';
 import reducer from './modules/reactml';
+import {
+	reactmlFieldChangeHandler,
+	dispatchFieldUpdate,
+	fromYaml, toYaml,
+} from './modules/reactml/util';
+
+import materialUiTagFactory from './components/reactml/materialUiTagFactory';
 
 export default {
-  ReactML, ReactMLHoc, reducer, codegen,
+  ReactML, ReactMLHoc, reducer,
+  materialUiTagFactory,
+  reactmlFieldChangeHandler, fromYaml, toYaml, dispatchFieldUpdate
 };
